14f25d1796003d9299c286523a59fa19c4a9bb95
B20DCPT094 <wuochuy02@gmail.com>
2023-01-05 10:43:15 +0700

    first commit

---
+++ final/final1.2/alert.js
@@ -0,0 +1,8 @@
+$(".btn-openPopup").on('click', function() {
+    $(".custom-model-main").addClass('model-open');
+  }); 
+  $(".close-btn, .bg-overlay").click(function(){
+    $(".custom-model-main").removeClass('model-open');
+  });
+  
+  
---
+++ final1.2/load.js
@@ -0,0 +1,8 @@
+var loader = function() {
+    setTimeout(function() {
+        $('#loader').css({ 'opacity': 0, 'visibility':'hidden' });
+    }, 1000);
+};
+$(function(){
+    loader();
+});
